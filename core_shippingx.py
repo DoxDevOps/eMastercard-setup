31ad64dcf1711fde558f27eb6e3fe7ce63f8c2ef
Billz <58287516+billz94@users.noreply.github.com>
2021-07-30 20:44:22 +0200

    Update core_shippingx.py
    cluster 27

--- core_shippingx.py
+++ core_shippingx.py
@@ -26,10 +26,10 @@ def alert(url, params):
     r = requests.post(url, json=params, headers=headers)
     return r
 
-recipients = ["+265998006237", "+265991450316", "+265995246144", "+265992182669", "+265995532195", "+265888890352", "+265884642428", "+265991852093"]
+recipients = ["+265998006237", "+265991450316", "+265995246144", "+2658822233319", "+265999959499"]
 
 #* Get cluster details
-cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/13')
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/27')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
